6ebaba5bae0f876598c1e07c5707daeb03e56a76
Ochieng John Ronnie <johnronnie18@gmail.com>
2023-09-24 12:11:52 +0300

    task 2

---
+++ index1.js
@@ -0,0 +1,23 @@
+//Write a program that takes the speed of a car as input e.g 80.
+// If the speed is less than 70, it should print “Ok”.
+// Otherwise, for every 5 km/s above the speed limit (70), 
+//it should give the driver one demerit point and print the total number of demerit points.
+
+// For example, if the speed is 80, it should print: “Points: 2”.
+// If the driver gets more than 12 points, the function should print: “License suspended”
+
+function speedDetector(speed){
+    let speedLimit = 70
+     points = (speed/5) - (speedLimit/5);
+    if (speed < 70){
+    return "ok";
+    } else if ( speed > 70 && points < 13){
+    for (let point = 0; point < 13; point ++){
+        return points;
+    } }
+    else (speed > 130)
+    points > 13;
+    return "License Suspended."
+}
+console.log(speedDetector (130));
+  
